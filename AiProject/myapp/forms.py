06d99c405dd6e270b16aa575ee493d8090522e21
BraaOkbe42 <braaokbe42@gmail.com>
2024-07-15 21:05:12 +0300

    BSPMS2410-6 regiseration for practitioner

---
+++ AiProject/myapp/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User, Student, Practitioner
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+
+class PractitionerSignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text='First name')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Last name')
+    email = forms.EmailField(max_length=254, required=True, help_text='Email')
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Register'))
+
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        username = cleaned_data.get('username')
+
+        if password1 and password2 and password1 != password2:
+            self.add_error('password2', "Passwords do not match.")
+
+        if User.objects.filter(username=username).exists():
+            self.add_error('username', "A user with that username already exists.")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_practitioner = True
+        if commit:
+            user.save()
+            Practitioner.objects.create(user=user)
+        return user
+
+
+
