b891322e81c8135044858a8206240d25591c0656
BraaOkbe42 <braaokbe42@gmail.com>
2024-07-15 21:15:45 +0300

    BSPMS2410-20 change password

--- AiProject/myapp/views.py
+++ AiProject/myapp/views.py
@@ -37,5 +37,15 @@ def practitioner_register(request):
 
 
 
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'change_password.html'
+    success_url = reverse_lazy('change_password')
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Your password was successfully updated!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Please correct the error below.')
+        return super().form_invalid(form)
 	
